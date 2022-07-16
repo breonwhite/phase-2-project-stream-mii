f1d5ea482be4e15876aad8e54cd6aba1f628a56f
Breon White <bdwhite92@gmail.com>
2022-07-16 10:09:53 -0400

    Added navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ShowForm from './components/shows/ShowForm'
 import ShowList from './components/shows/ShowList'
 import Home from './components/static/Home'
+import Navbar from './components/navigation/Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/shows/new" element={<ShowForm />} />
         <Route path="/shows" element={<ShowList />} />
--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <div>
+        <ul>
+            <li><Link to="/shows/new">ADD SHOWS</Link></li>
+            <li><Link to="/shows">YOUR SHOWS</Link></li>
+            <li><Link to="/">HOME</Link></li>
+        </ul>
+    </div>
   )
 }
 
