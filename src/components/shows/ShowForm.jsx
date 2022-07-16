c3c3ee9be444b2cb192b59dbb3b7a8182df6fb2a
Breon White <bdwhite92@gmail.com>
2022-07-16 13:52:20 -0400

    Added fetch request for show results

--- src/components/shows/ShowForm.jsx
+++ src/components/shows/ShowForm.jsx
@@ -4,10 +4,51 @@ import { useState } from 'react'
 const ShowForm = () => {
     const [ show, setShow ] = useState('');
     const [ platform, setPlatform ] = useState('');
+    const [ searchResult, setSearchResults ] = useState({});
 
-    const handleSubmit = e => {
+    async function fetchShows() {
+        const customURL = ('https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=' 
+        + platform + '&type=series&keyword=' + show
+        + '&output_language=en').replace(/ /g, '%20')
+
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '85900b7c00mshcf991f3bb28992ep180ad5jsn97819322cdfa',
+                'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+            }
+        };
+        
+        const response = await fetch(customURL, options);
+        const shows = await response.json();
+        return shows
+    }
+
+    
+    
+    
+    const handleSearch = e => {
         e.preventDefault();
         console.log(show, platform)
+        
+        fetchShows().then(data => {
+            const results = data.results;
+            const returnedShows = results.map(result => {
+                let showObj = {
+                    "title" : result.title,
+                    "runtime": result.episodeRuntimes[0],
+                    "seasons": result.seasons,
+                    "episodes": result.episodes,
+                    "poster": result.posterURLs.original,
+                    "overview": result.overview,
+                    "streaming": platform,
+                    "rating": result.tmdbRating,
+                    "video": result.video
+                }
+                return showObj;
+            })
+            setSearchResults(returnedShows)
+        })
     }
   
     return (
@@ -28,7 +69,7 @@ const ShowForm = () => {
                     </select>
             </div>
             
-            <input type="button" value="Search Your Favorite Show" onClick={handleSubmit} />
+            <input type="button" value="Search Your Favorite Show" onClick={handleSearch} />
         </form>
     </div>
   )
