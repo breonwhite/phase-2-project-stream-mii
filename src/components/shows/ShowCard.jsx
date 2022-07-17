bffe6be4c8778841f1bc8431180e044368667c22
Breon White <bdwhite92@gmail.com>
2022-07-17 12:03:31 -0400

    Added style format for cards

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import { useState } from 'react'
 
+import { useTheme } from '@mui/material/styles';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import InfoIcon from '@mui/icons-material/Info';
+
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Box from '@mui/material/Box';
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
 const ShowCard = ({ show, saveShow, unsaveShow }) => {
   const [ save, setSave ] = useState(false)
   const [ day, setDay ] = useState('wednesday')
+
+  const theme = useTheme();
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
     
   const handleSave = () => {
     saveShow(show, day)
@@ -16,18 +64,69 @@ const ShowCard = ({ show, saveShow, unsaveShow }) => {
   }
     
   return (
-    <li>
-        { show.title }
-        { show.runtime }
-                    <select name="day" id="day" onChange={ e => setDay( e.target.value )}>
-                    <option value="monday">Monday</option>
-                    <option value="tuesday">Tuesday</option>
-                    <option value="wednesday">Wednesday</option>
-                    <option value="thursday">Thursday</option>
-                    <option value="friday">Friday</option>
-                    </select>
-            { save ? <button onClick={ handleUnsave }>Unsave Show</button> : <button onClick={ handleSave }>Add Show</button> }
-    </li>
+    <Card sx={{ display: 'flex', width: '100%' }}>
+        <CardMedia
+        component="img"
+        sx={{ width: 151, }}
+        image={show.poster}
+        alt="Live from space album cover"
+      />
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            {show.title}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            Available for Streaming on Hulu
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+       <IconButton aria-label="add to favorites">
+         <FavoriteIcon />
+       </IconButton>
+       <IconButton aria-label="share">
+         <ShareIcon />
+       </IconButton>
+      <ExpandMore
+        expand={expanded}
+        onClick={handleExpandClick}
+        aria-expanded={expanded}
+        aria-label="show more"
+      >
+        <ExpandMoreIcon />
+      </ExpandMore>
+    </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+            aside for 10 minutes.
+          </Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+            large plate and set aside, leaving chicken and chorizo in the pan. Add
+            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+            stirring often until thickened and fragrant, about 10 minutes. Add
+            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and
+            peppers, and cook without stirring, until most of the liquid is absorbed,
+            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+            mussels, tucking them down into the rice, and cook again without
+            stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don&apos;t open.)
+          </Typography>
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          </Typography>
+        </CardContent>
+      </Collapse>
+      </Box>
+    </Card>
   )
 }
 
